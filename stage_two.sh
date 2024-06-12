668ec6ca487896507d8e8073bef1e72f66838886
Josh Thor <jthor@42kl.edu.my>
2024-06-12 16:08:34 +0800

    feat: added stage two

---
+++ stage_two.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+/usr/local/bin/code ~/Desktop/fugee_pygame && /usr/bin/open -a "Google Chrome.app" "https://gilded-blue-2ea.notion.site/Part-2-Game-Development-with-Pygame-5dd0b86095674b3599a46c32a8eb2eff?pvs=4"
