b1ed860dffff05a01236f427ffe9917c88a24a75
Josh Thor <jthor@42kl.edu.my>
2024-06-12 14:41:54 +0800

    fix: stage one opens the right directory.

--- stage_one.sh
+++ stage_one.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-/usr/local/bin/code ~/Desktop/fugee_pygame && /usr/bin/open -a "Google Chrome.app" "https://gilded-blue-2ea.notion.site/Part-1-Introduction-to-Python-0e717a573c75426aa9f109de9e918b30?pvs=4"
+/usr/local/bin/code ~/Desktop/intro_to_python && /usr/bin/open -a "Google Chrome.app" "https://gilded-blue-2ea.notion.site/Part-1-Introduction-to-Python-0e717a573c75426aa9f109de9e918b30?pvs=4"
