dea292dd35f803cb86992a2ce7a64c2332622ae5
Josh Thor <jthor@42kl.edu.my>
2024-09-09 08:51:55 +0800

    chore: removed youtube link

--- automation-with-python.sh
+++ automation-with-python.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-/usr/bin/open -a "Google Chrome.app" "https://fx-digitalskills.notion.site/SFX-Automation-With-Python-54d6d88a6c394660982280a4b4bccf89?pvs=4" && /usr/bin/open -a "Google Chrome.app" "https://www.youtube.com/playlist?list=PLPyCdGJo2_ZoMqDcF7zAJy066mo4n6F3p"
+/usr/bin/open -a "Google Chrome.app" "https://fx-digitalskills.notion.site/SFX-Automation-With-Python-54d6d88a6c394660982280a4b4bccf89?pvs=4"
